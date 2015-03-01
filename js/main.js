5fdc99253815de712d512deeeb52e2f575221fa0
Yuliana Mallorga <yulianamallorga@gmail.com>
2015-03-01 20:34:09 -0300

    [Modal] - Actualización de la estructura html - Actualización del javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
+var template= $('#template-modal').html();
+
+$('.open-menu').on('click', function(){
+	$('.sub-menu').toggleClass('hide');
+});
+
+
 $('.news').on('click', function(){
 	$('.news').removeClass('selected');
 	$(this).addClass('selected');
-	$('.modal').removeClass('hide');
 });
 
 $('.back-to-top').on('click', function(){
 	$('body').scrollTop(0);
 });
 
-$('button.close').on('click', function(){
-	$('.modal').toggleClass('hide');
-	$('.news').removeClass('selected');
-});
 
-$('.open-menu').on('click', function(){
-	$('.sub-menu').toggleClass('hide');
+$('.news').on('click', function(){
+	window.title = $(this).data('title');
+	window.img = $(this).data('img');
+	window.description = $(this).data('description');
+
+});
+	
+$('.news').fancybox({
+	content: template,
+	
+	beforeShow: function(){
+		$('.fancybox-overlay #modal')
+			.find('.news-title').html(window.title).end()
+			.find('.news-img img').attr('src', window.img).end()
+			.find('.news-description').html(window.description).end();
+	}
 });
